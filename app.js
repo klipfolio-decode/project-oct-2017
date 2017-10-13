4d84a0e9272d155d50dd50276b2e38e652adf78b
Brandon Danis <bdani093@uottawa.ca>
2017-10-13 13:50:49 -0400

    A bit of refactoring before starting

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
-
-const routes = require("./server/routes");
 const config = require("./config.json");
 
-routes.register(app);
+// set up static routes
+app.use(express.static("./www/html"));
+app.use("/js", express.static("./www/js"));
+app.use("/css", express.static("./www/css"));
+app.use("/build", express.static("./build"))
+
+app.get("/", function(req, res) {
+	res.render("index");
+});
+
+app.use("/api", require("./server/routes"))
 
 app.listen(config.port);

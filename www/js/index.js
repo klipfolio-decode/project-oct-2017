d88baf19a8845c9c5dfe063b1b8a9e58e6fed5b7
Abdullah Mostafa <amost076@uottawa.ca>
2017-10-13 17:38:30 -0400

    fixed merge conflict

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,8 @@
 import $ from "jquery";
 
-import * as viz from "./visualizations";
+//import * as viz from "./visualizations";
 
+/*
 const targetDivId = "container";
 const targetDiv = document.createElement("div");
 $(targetDiv)
@@ -11,6 +12,8 @@ $(targetDiv)
 
 viz.renderChart("myChart");
 viz.renderTo(targetDivId);
+
+*/
 if (window.addEventListener) window.addEventListener("load", new lexaudio.example(), false);
 else if (window.attachEvent) window.attachEvent("onload", setup(new lexaudio.example()));
 else window.onload = new lexaudio.example();

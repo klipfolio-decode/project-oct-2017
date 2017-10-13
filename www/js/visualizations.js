e1fead259d707765410629422441dc9a1f5ba17a
Laurence Li <li.laurence55@gmail.com>
2017-10-13 15:59:54 -0400

    removed unneeded lines

--- www/js/visualizations.js
+++ www/js/visualizations.js
@@ -1,52 +1,6 @@
 import $ from "jquery";
 import Chart from "chart.js";
 
-var mockData = {
-    "visualizations": [
-        {
-            "id": "1",
-            "type": "line",
-            "label": "name",
-            "data":{
-                "labels": ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                "datasets": [{
-                    "label": "# of Votes",
-                    "data": [12, 19, 3, 5, 2, 3],
-                    "borderColor": [
-                        "rgba(255,99,132,1)"],
-                    "borderWidth": 1
-                },{
-                    "label": "# of Whatever;",
-                    "data": [11, 18, 2, 4, 1, 2],
-                    "borderColor": [
-                        "rgba(255,99,132,1)"],
-                    "borderWidth": 1
-                }]
-            }
-        },
-        {
-            "id": "2",
-            "type": "line",
-            "label": "name2",
-            "data":{
-                "labels": ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                "datasets": [{
-                    "label": "# of Votes",
-                    "data": [5, 12, 10, 1, 26, 3],
-                    "borderColor": [
-                        "rgba(255,99,132,1)"],
-                    "borderWidth": 1
-                },{
-                    "label": "# of Whatever;",
-                    "data": [11, 18, 2, 4, 1, 2],
-                    "borderColor": [
-                        "rgba(255,99,132,1)"],
-                    "borderWidth": 1
-                }]
-            }
-        }
-    ]};
-
 export function renderAll(data){
     $("#visualizationContainer").html("")
 

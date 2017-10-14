3402ccc521faf3f7558d7b016a5c46d0aa98bd25
Brandon Danis <bdani093@uottawa.ca>
2017-10-13 20:16:30 -0400

    Adding colorful logging

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const config = require("./config.json");
 const bodyParser = require('body-parser')
+const colors = require('colors')
 
 app.use(bodyParser.json())
 
@@ -18,3 +19,8 @@ app.get("/", function(req, res) {
 app.use("/api", require("./server/routes"))
 
 app.listen(config.port);
+
+console.log("=====================================".bold.red)
+console.log("====  SERVER OPEN FOR BUSINESS   ====".bold.red);
+console.log(`====         PORT ${config.port}           ====`.bold.red);
+console.log("=====================================".bold.red)

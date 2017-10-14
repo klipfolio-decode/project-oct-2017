2aeb9e2feb03914b0211d261aaaed80510d8f8e1
Abdullah Mostafa <amost076@uottawa.ca>
2017-10-14 15:47:53 -0400

    Convo Fix, Remove tool tip

---
+++ www/js/convo.js
@@ -0,0 +1,118 @@
+
+// set the focus to the input box
+document.getElementById("m_quicksearch_input").focus();
+
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+// Provide your Pool Id here
+IdentityPoolId:'us-east-1:6a5cf4b0-5e5c-4f5d-b5bd-d66dea8de13c',
+});
+
+var lexruntime = new AWS.LexRuntime();
+var lexUserId = 'svrk8aedlgyokzngyrldrgx2kwade1l1' + Date.now();
+var sessionAttributes = {};
+
+function pushChat() {
+
+    // if there is text to be sent...
+    var wisdomText = document.getElementById("m_quicksearch_input");
+    if (wisdomText && wisdomText.value && wisdomText.value.trim().length > 0) {
+
+        // disable input to show we're sending it
+        var wisdom = wisdomText.value.trim();
+        wisdomText.value = '...';
+        wisdomText.locked = true;
+
+        // send it to the Lex runtime
+        var params = {
+            botAlias: '$LATEST',
+            botName: 'decode',
+            inputText: wisdom,
+            userId: lexUserId,
+            sessionAttributes: sessionAttributes
+        };
+        showRequest(wisdom);
+        lexruntime.postText(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack);
+                showError('Error:  ' + err.message + ' (see console for details)')
+            }
+            if (data) {
+                // capture the sessionAttributes for the next cycle
+                sessionAttributes = data.sessionAttributes;
+                // show response and/or error/dialog status
+                console.log(data)
+                let params = {
+                    'slots': data.slots,
+                    'inputTranscript': data.inputTranscript
+                }
+                if (params.slots.metric){
+                  $.ajax(
+                    {
+                      method: 'POST',
+                      contentType: 'application/json',
+                      url: '/api/visualizations',
+                      data: JSON.stringify(params)
+                    }
+                  ).done( function(){
+                    console.log('sent')
+                  })
+                }
+
+                showResponse(data);
+            }
+            // re-enable input
+            wisdomText.value = '';
+            wisdomText.locked = false;
+        });
+    }
+    // we always cancel form submission
+    return false;
+}
+
+function showRequest(daText) {
+
+    var conversationDiv = document.getElementById('conversation');
+    var requestPara = document.createElement("div");
+
+    requestPara.setAttribute('class','userRequest m-alert m-alert--outline alert alert-brand alert-dismissible fade show');    
+
+    requestPara.appendChild(document.createTextNode(daText));
+    conversationDiv.appendChild(requestPara);
+    conversationDiv.scrollTop = conversationDiv.scrollHeight;
+}
+
+function showError(daText) {
+
+    var conversationDiv = document.getElementById('conversation');
+    var errorPara = document.createElement("P");
+    responsePara.setAttribute('class','lexError alert alert-danger');    
+
+
+    errorPara.appendChild(document.createTextNode(daText));
+    conversationDiv.appendChild(errorPara);
+    conversationDiv.scrollTop = conversationDiv.scrollHeight;
+}
+
+function showResponse(lexResponse) {
+
+    var conversationDiv = document.getElementById('conversation');
+    var responsePara = document.createElement("div");
+    responsePara.setAttribute('class','lexResponse alert alert-brand');    
+
+    if (lexResponse.message) {
+        responsePara.appendChild(document.createTextNode(lexResponse.message));
+        responsePara.appendChild(document.createElement('br'));
+    }
+    if (lexResponse.dialogState === 'ReadyForFulfillment') {
+        responsePara.appendChild(document.createTextNode(
+            'Ready for fulfillment'));
+        // TODO:  show slot values
+    } else {
+        responsePara.appendChild(document.createTextNode(
+            '(' + lexResponse.dialogState + ')'));
+    }
+    conversationDiv.appendChild(responsePara);
+    conversationDiv.scrollTop = conversationDiv.scrollHeight;
+}
